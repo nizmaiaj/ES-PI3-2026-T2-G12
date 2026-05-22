f5f426e290986bd86320c306131eec7d4f43cd99
Gabriel Henrique Pozeti de Faria <ghfaria@buckman.com>
2026-05-22 16:59:43 -0300

    Consertada a autenticacao de dois fatores

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -14,6 +16,13 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  if (!kIsWeb &&
+      kDebugMode &&
+      defaultTargetPlatform == TargetPlatform.android) {
+    // Local debug builds are not Play Store-distributed, so test Phone Auth via reCAPTCHA.
+    await FirebaseAuth.instance.setSettings(forceRecaptchaFlow: true);
+  }
 
   runApp(const MeuApp());
 }
